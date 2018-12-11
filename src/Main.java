96c793049bfce6e94bbe30af8b305fb1ce76fdbc
Monpocchi <rlnd.nguyen@googlemail.com>
2018-12-11 13:22:57 +0100

    Implement Hello World program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	/**
+	 * @wi.implements ITU18-10587 Simple function that prints 'Hello World!'
+	 */
+	public static void main(String... args) {
+		System.out.println("Hello World!");
+	}
+}
