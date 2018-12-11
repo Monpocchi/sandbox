272192f22d427e0c11a6920e60e62625a1f206b0
Monpocchi <rlnd.nguyen@googlemail.com>
2018-12-11 14:37:47 +0100

    Change linking ID

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	/**
-	 * @wi.implements ITU18-10587 Simple function that prints 'Hello World!'
+	 * @wi.implements PF-9913 Simple function that prints 'Hello World!'
 	 */
 	public static void main(String... args) {
 		System.out.println("Hello World!");
